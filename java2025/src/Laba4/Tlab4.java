7901d8fc120acbd1a28b6c395631308f60aa6044
lups-1 <vvp230506@gmail.com>
2025-12-11 22:46:09 +0100

    Update Tlab4.java

--- java2025/src/Laba4/Tlab4.java
+++ java2025/src/Laba4/Tlab4.java
@@ -1,18 +1,1 @@
-package Laba4;
 
-import java.util.Arrays;
-
-public class Tlab4 {
-    public static void main(String[] args) {
-        int [] array = {4, 3, 1, 2};
-        System.out.println(Arrays.toString(array));
-        System.out.println(sum(array));
-    }
-    public static double sum(int[] arr) {
-        double sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        return sum/arr.length;
-    }
-}
